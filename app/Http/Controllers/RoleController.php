ed1f8b8812aab19f852d785cd45e26f1263618b1
elinatavakkoli <rahbartalab2002@gmail.com>
2023-02-01 16:57:33 +0330

    index rules completed - CR in CRUD completed

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -9,14 +9,10 @@ use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     */
     public function index()
     {
         return view('roles.index', [
-            "roles" => Role::all()
+            "roles" => Role::filter()->get()
         ]);
     }
 
@@ -27,11 +23,7 @@ class RoleController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     */
+
     public function store(CreateRoleRequest $request)
     {
         /* @var $role Role */
@@ -41,46 +33,25 @@ class RoleController extends Controller
         return redirect(route('roles.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
