936be41afed6cccfe63477aea6ecd943ff72c182
rahbartalab <rahbartalab2002@gmail.com>
2023-02-03 19:41:14 +0330

    optimize User Register with casting

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -19,13 +19,7 @@ class RegisterUserController extends Controller
 
     public function store(RegisterUserRequest $request)
     {
-        User::create([
-            'first_name' => $request['first_name'],
-            'last_name' => $request['last_name'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password'])
-        ]);
-
+        User::create($request->validated());
 
         return redirect(route('login'));
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use App\Casts\Auth\PasswordCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -83,6 +83,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => PasswordCast::class
     ];
 
 
