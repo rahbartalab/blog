c95ba46e6bad0be88ae6d8b39d2ab8cc28c63e14
rahbartalab <rahbartalab2002@gmail.com>
2023-02-04 13:12:42 +0330

    add filter by permissions for show Roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Models\Role as SpatieRole;
@@ -16,7 +17,15 @@ class Role extends SpatieRole
 
     public static function scopeFilter($query)
     {
-        $query->when(\request('name') ?? false, fn($query) => $query->where('name', 'LIKE', '%' . \request('name') . '%'));
+        /* --!> name filter <!-- */
+        $query->when(\request('name') ?? false,
+            fn($query) => $query->where('name', 'LIKE', '%' . \request('name') . '%'));
+
+        /* --!> permissions filter <!-- */
+        $query->when(request('permissions') ?? false,
+            fn($query) => $query->whereHas('permissions',
+                fn($query) => $query->whereIn('id', request('permissions'))
+            ));
     }
 
 }
